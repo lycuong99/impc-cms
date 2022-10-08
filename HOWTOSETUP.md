e4cdf6b4cda8e902cd81550ce21c188181f2991d
cuong <lycuong99@gmail.com>
2022-10-08 18:46:38 +0700

    up

---
+++ HOWTOSETUP.md
@@ -0,0 +1,38 @@
+# STEP 1: Install package
+```
+npm i
+```
+# STEP 2: Config
+config database in config/database.js
+
+create database manually
+
+IF USE POSTGRE
+```
+npm i pg
+```
+IF USE POSTGRE
+```
+npm i mysql
+```
+
+config env in .env
+
+# STEP 3: Build & Start
+```
+npm run build
+```
+
+Start your Strapi application with autoReload enabled
+```
+npm run develop
+```
+Start your Strapi application with autoReload disabled.
+```
+npm run start
+```
+# STEP 4: Register Admin account
+open URL and register
+
+
+# STEP 5: Migrate Content and Media File
--- config/database.js
+++ config/database.js
@@ -1,11 +1,48 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
     useNullAsDefault: true,
   },
 });
+
+// // MY SQL
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 3306),
+//       database: env("DATABASE_NAME", "impc-strapi"),
+//       user: env("DATABASE_USERNAME", "root"),
+//       password: env("DATABASE_PASSWORD", "Admin@1234"),
+//       ssl: {
+//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
+// POSTGRES
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "impc-db-01"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "123456"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
